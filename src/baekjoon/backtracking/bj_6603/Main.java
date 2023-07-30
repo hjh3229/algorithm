1f576f8a6744555e38c34d511b2fc4dc90c870d9
Han JiHun <hjh_3229@naver.com>
2023-07-30 21:38:42 +0900

    백트래킹 문제 발생

---
+++ src/baekjoon/backtracking/bj_6603/Main.java
@@ -0,0 +1,45 @@
+package baekjoon.backtracking.bj_6603;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  static StringBuilder sb = new StringBuilder();
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int k;
+  static int[] S;
+  static int[] a = new int[6];
+  static boolean[] b;
+  public static void main(String[] args) throws Exception {
+    while (true) {
+      k = Integer.parseInt(br.readLine());
+      if (k == 0) {
+        return;
+      }
+      S = new int[k];
+      b = new boolean[k];
+      String[] s = br.readLine().split(" ");
+      for (int i = 0; i < k; i++) {
+        S[i] = Integer.parseInt(s[i]);
+      }
+      solution(0);
+      System.out.println(sb);
+    }
+  }
+
+  public static void solution(int index) throws Exception {
+    if (index == 6) {
+      for (int i = 0; i < 6; i++) {
+        sb.append(a[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+    for (int i = 0; i < k; i++) {
+      if (b[i]) continue;
+      b[i] = true;
+      a[index] = S[i];
+      solution(index+1);
+    }
+  }
+}
