c609d4d21526fed10792c31737e493504cff65a8
HanJiHun <hjh_3229@naver.com>
2023-11-07 17:28:39 +0900

    bj_11003

---
+++ src/baekjoon/sum/bj_11003.py
@@ -0,0 +1,15 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, l = map(int, input().split())
+mydeque = deque()
+now = list(map(int, input().split()))
+
+for i in range(n):
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i], i))
+    if mydeque[0][1] <= i - l:
+        mydeque.popleft()
+    print(mydeque[0][0], end=' ')
