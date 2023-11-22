c46c8b2072174630c5257c2221f2583fdb51cf94
HanJiHun <hjh_3229@naver.com>
2023-11-22 14:55:31 +0900

    bj_9498

---
+++ src/baekjoon/calculate/bj_9498.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a > 100 or a < 60:
+    print("F")
+elif a >= 90:
+    print("A")
+elif a >= 80:
+    print("B")
+elif a >= 70:
+    print("C")
+elif a >= 60:
+    print("D")
