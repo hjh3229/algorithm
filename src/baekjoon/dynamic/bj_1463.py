bee828d015545950ae6d8f292fc4f3c059fefe3e
HanJiHun <hjh_3229@naver.com>
2024-01-03 16:32:56 +0900

    오답 노트

--- src/baekjoon/dynamic/bj_1463.py
+++ src/baekjoon/dynamic/bj_1463.py
@@ -18,3 +18,32 @@ def cal(n):
     return root[n]
 
 print(cal(n))
+
+# 시간 초과
+# N = int(input())
+
+# root = [0] * 1000001
+
+# def cal(n):
+#     if n == 1:
+#         return
+#     if n % 3 == 0:
+#         if n % 2 == 0:
+#             cal(n // 3)
+#             cal(n // 2)
+#             root[n] = min(root[n // 3], root[n // 2]) + 1
+#         else:
+#             cal(n // 3)
+#             cal(n - 1)
+#             root[n] = min(root[n // 3], root[n - 1]) + 1
+#     else:
+#         if n % 2 == 0:
+#             cal(n // 2)
+#             cal(n - 1)
+#             root[n] = min(root[n // 2], root[n - 1]) + 1
+#         else:
+#             cal(n - 1)
+#             root[n] = root[n - 1] + 1
+
+# cal(N)
+# print(root[N])
