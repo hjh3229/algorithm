e6ef4dd780b352a339645c20c37bd2bedc17d8f6
HanJiHun <hjh_3229@naver.com>
2024-01-26 14:32:39 +0900

    bj_1629

---
+++ src/baekjoon/divide and conquer/bj_1629.py
@@ -0,0 +1,13 @@
+A, B, C = map(int, input().split())
+
+def multi (a, n):
+  if n == 1:
+      return a % C
+  else:
+      tmp = multi(a, n//2)
+      if n % 2 == 0:
+          return (tmp * tmp) % C
+      else:
+          return (tmp  * tmp * a) % C
+          
+print(multi(A, B))
